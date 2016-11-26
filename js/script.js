ddb420bae596c31e5d6fcdbad4ffe3e12581dd64
mohamme dabdellatief <mabdellatief@gmail.com>
2016-11-26 23:06:01 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,60 +1,99 @@
-// Apply on desktop only not mobile (no heavy animation on mobile)
-if ( !/Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)  &&  $(window).width() >= 1024) {
+(function(window,$){
+
+    // wait for the document ready
+    $(document).ready(function() {
+
+        // fixed nav-bar on scroll
+        var nav = $(".navbar-fixed-top");
+        $(window).scroll(function() {
+            if (nav.offset().top > 200) {
+                nav.addClass("top-nav-expand");
+                //alert('hi');
+            } else {
+                nav.removeClass("top-nav-expand");
+            }
+        });
+
+        /**
+         * This part causes smooth scrolling using scrollto.js
+         * We target all a tags inside the nav, and apply the scrollto.js to it.
+         */
+        $("nav a").click(function(evn){
+            evn.preventDefault();
+            $('html,body').scrollTo(this.hash, this.hash);
+        });
 
 
-    var nav = $(".navbar-fixed-top");
-    //jQuery to collapse the navbar on scroll
-    $(window).scroll(function() {
-        if (nav.offset().top > 500) {
-            nav.removeClass("top-nav-collapse");
-            //alert('hi');
-        } else {
-            nav.addClass("top-nav-collapse");
+
+        /**
+         * This part handles the highlighting functionality.
+         * We use the scroll functionality again, some array creation and
+         * manipulation, class adding and class removing, and conditional testing
+         */
+        var aChildren = $("nav li").children(); // find the a children of the list items
+        var aArray = []; // create the empty aArray
+        for (var i=0; i < aChildren.length; i++) {
+            var aChild = aChildren[i];
+            var ahref = $(aChild).attr('href');
+            aArray.push(ahref);
+        } // this for loop fills the aArray with attribute href values
+
+        $(window).scroll(function(){
+            var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+            var windowHeight = $(window).height(); // get the height of the window
+            var docHeight = $(document).height();
+
+            for (var i=0; i < aArray.length; i++) {
+                var theID = aArray[i];
+                var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+                var divHeight = $(theID).height(); // get the height of the div in question
+                if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                    $("a[href='" + theID + "']").addClass("nav-active");
+                } else {
+                    $("a[href='" + theID + "']").removeClass("nav-active");
+                }
+            }
+
+            if(windowPos + windowHeight == docHeight) {
+                if (!$("nav li:last-child a").hasClass("nav-active")) {
+                    var navActiveCurrent = $(".nav-active").attr("href");
+                    $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+                    $("nav li:last-child a").addClass("nav-active");
+                }
+            }
+        });
+
+
+
+        // Contact form
+        //=====================================================
+        $('input, textarea').blur(function() {
+            var $this = $(this);
+            if ($this.val())
+                $this.addClass('used');
+            else
+                $this.removeClass('used');
+        });
+
+        //hamburger menu
+        //=====================================================
+        $(".hamburger").on('click',function(e){
+            e.preventDefault();
+            $(this).toggleClass('is-active');
+        });
+        $(".site-overlay, .pushy-link").on('click',function(e){
+            e.preventDefault();
+            $('.hamburger').removeClass('is-active');
+        });
+
+
+
+        // Apply on desktop only not mobile (no heavy animation on mobile)
+        if ( !/Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)  &&  $(window).width() >= 768) {
+
         }
-    });
 
-
-}
-$(document).ready(function() {
-
-
-    // google map dime on and click to activate
-    $(".map_overlay").on('click',function () {
-        $('.google-maps').removeClass('scrolloff');
-        $('.map_overlay').fadeOut();
 
     });
 
-    $(".sixth_sec").on('mouseleave',function () {
-        $('.google-maps').addClass('scrolloff');
-        $('.map_overlay').fadeIn();
-    });
-
-
-
-    // Contact form
-    //=====================================================
-    $('input, textarea').blur(function() {
-        var $this = $(this);
-        if ($this.val())
-            $this.addClass('used');
-        else
-            $this.removeClass('used');
-    });
-
-    //hamburger menu
-    $(".hamburger").on('click',function(e){
-        e.preventDefault();
-        $(this).toggleClass('is-active');
-    });
-    $(".site-overlay, .pushy-link").on('click',function(e){
-        e.preventDefault();
-        $('.hamburger').removeClass('is-active');
-    });
-
-
-});
-
-
-
-
+})(window,jQuery);
